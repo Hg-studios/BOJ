d0f42ab4f84c4e195b3af63af104b8c780ca1ec3
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2022-03-07 23:48:51 +0900

    Update 10430.cc

--- 10430.cc
+++ 10430.cc
@@ -5,14 +5,17 @@ using namespace std;
 int main(){
   int a,b,c;
 
+  
   cin >> a;
   cin >> b;
   cin >> c;
 
+  
   cout << (a+b)%c << '\n';
   cout << ((a+c)+(b%c))%c << '\n';
   cout << (a*b)%c << '\n';
   cout << ((a%c)*(b%c))%c << '\n';
 
+  
   return 0;
 }
