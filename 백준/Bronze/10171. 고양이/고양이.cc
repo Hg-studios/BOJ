efdd0e3bbcc39429b4a7bb6ffe1c87c76ac6f208
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-01-29 15:12:55 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+	cout << "\\    /\\" << '\n'
+		<< " )  ( \')" << '\n'
+		<< "(  /  )" << '\n'
+		<< " \\(__)|";
+	return 0;
+}
