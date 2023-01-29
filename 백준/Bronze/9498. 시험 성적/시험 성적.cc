0e9e25318772144e37adffcf54e19b65e0a09fef
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-01-29 15:20:55 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+	int s; cin >> s;
+	
+	if (s >= 90 && s <= 100) cout << "A";
+	else if (s >= 80 && s < 90) cout << "B";
+	else if (s >= 70 && s < 80) cout << "C";
+	else if (s >= 60 && s < 70) cout << "D";
+	else cout <<"F";
+
+	return 0;
+}
