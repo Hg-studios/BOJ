88850cce974f2c44bac2345e088a6a62cd1a50ba
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-01-30 16:42:05 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n; cin >> n;
+
+	for (int i = n; i >= 1; i--) {
+		for (int k = 1; k <= n - i; k++) cout << " ";
+		for (int j = 1; j <= 2*i-1; j++) cout << "*";
+		
+		cout << '\n';
+	}
+	return 0;
+}
