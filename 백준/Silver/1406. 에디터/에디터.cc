8bcbf170e36967e91c65fdf10d5332e8395fbdc0
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-02-02 23:57:46 +0900

    [Silver II] Title: 에디터, Time: 68 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,62 +1,36 @@
-#include <iostream>
-#include <stack>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-  stack<char> l; //커서 기준 왼쪽 문자열 저장 스택
-  stack<char> r; //커서 기준 오른쪽 문자열 저장 스택
-
-  string str;
-  cin >> str;
-
-  for(char ch : str){ //읽은 str의 글자 하나하나를 l 스택에 저장
-    l.push(ch);
-  }
-  
-  int n;
-  cin >> n;
-  cin.ignore();
-  
-  while(n--){
-    string cmd;
-    getline(cin, cmd);
-    
-    if(cmd == "L"){
-      if(!l.empty()){
-        char ch1 = l.top(); //가장 위에 있는 걸 꺼내서
-        r.push(ch1); //r로 옮겨주고
-        l.pop(); //가장 위에 것을 삭제해줌
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string init;
+  cin >> init;
+  list<char> L;
+  for (auto c : init) L.push_back(c);
+  auto cursor = L.end();
+  int q;
+  cin >> q;
+  while (q--) {
+    char op;
+    cin >> op;
+    if (op == 'P') {
+      char add;
+      cin >> add;
+      L.insert(cursor, add);
+    }
+    else if (op == 'L') {
+      if (cursor != L.begin()) cursor--;
+    }
+    else if (op == 'D') {
+      if (cursor != L.end()) cursor++;
+    }
+    else { // 'B'
+      if (cursor != L.begin()) {
+        cursor--;
+        cursor = L.erase(cursor);
       }
     }
-    else if(cmd == "D"){
-      if(!r.empty()){
-        char ch2 = r.top();
-        l.push(ch2);
-        r.pop();
-      }
-    }
-    else if(cmd == "B"){
-      if(!l.empty()){
-        l.pop();
-      }
-    }
-    else if(cmd[0] == 'P'){
-      l.push(cmd[2]);
-    }
   }
-
-  while(!l.empty()){
-    char ch = l.top();
-    l.pop();
-    r.push(ch);
-  }
-
-  while(!r.empty()){
-    cout << r.top();
-    r.pop();
-  }
-  cout << '\n';
-  
-  return 0;
+  for (auto c : L) cout << c;
 }
