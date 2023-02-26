167e0237185357b66c105b4eaa207494b64309ce
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-02-26 17:51:23 +0900

    [Gold IV] Title: N-Queen, Time: 1672 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,32 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-bool isused1[40]; // column을 차지하고 있는지
-bool isused2[40]; // / 방향 대각선을 차지하고 있는지
-bool isused3[40]; // \ 방향 대각선을 차지하고 있는지
 
-int cnt = 0;
-int n;
-void func(int cur) { // cur번째 row에 말을 배치할 예정임
-  if (cur == n) { // N개를 놓는데 성공했다면
-    cnt++;
-    return;
-  }
-  for (int i = 0; i < n; i++) { // (cur, i)에 퀸을 놓을 예정
-    if (isused1[i] || isused2[i+cur] || isused3[cur-i+n-1]) // column이나 대각선 중에 문제가 있다면
-      continue;
-    isused1[i] = 1;
-    isused2[i+cur] = 1;
-    isused3[cur-i+n-1] = 1;
-    func(cur+1);
-    isused1[i] = 0;
-    isused2[i+cur] = 0;
-    isused3[cur-i+n-1] = 0;
-  }
+bool isused1[40];
+bool isused2[40];
+bool isused3[40];
+
+int cnt, n;
+
+void func(int cur) { //cur번째 row에 말을 배치할 예정임
+	if (cur == n) { //n개의 퀸을 놓는데 성공한 것임
+		cnt++;
+		return;
+	}
+
+	for (int i = 0; i < n; i++) { //(cur, i)에 퀸을 놓을 예정임
+		//열이나 대각선 중에 충돌이 있다면 놓지 못함 
+		if (isused1[i] || isused2[i + cur] || isused3[cur - i + n - 1]) continue;
+
+		isused1[i] = 1;
+		isused2[i + cur] = 1;
+		isused3[cur - i + n - 1] = 1;
+		func(cur + 1);
+		isused1[i] = 0;
+		isused2[i + cur] = 0;
+		isused3[cur - i + n - 1] = 0;
+	}
 }
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n;
-  func(0);
-  cout << cnt;
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	func(0);
+	cout << cnt;
+	
+	return 0;
 }
