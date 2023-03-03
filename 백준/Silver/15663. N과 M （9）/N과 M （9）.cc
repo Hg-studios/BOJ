4f287e677678627f5217996ff1e6adbca03a11a4
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-03-03 17:29:50 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -2,56 +2,39 @@
 #include <algorithm>
 using namespace std;
 
-int arr[10];
-int input[10]; //input에 주어진 n개의 수를 저장함
-bool isused[10];
-int n, m;
-
-void func(int k) {
-	if (k == m) { //모든 수를 다 넣은 경우이므로 끝임 
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << " ";
-		}
-		cout << '\n';
-		return;
-	}
-
-	//Q : 예제2에서 9 9는 어떻게 나오는 거임??...
-	//A : func(k+1)로 들어가게 되면 tmp가 또 하나 생김. 그리고 걔는 0으로 초기화되고..
-	//	  그래서 같은 레벨에 있는 즉, 같은 k 레벨끼리만 중복을 검사하게 되는 것임
-
-	int tmp = 0; //중복 수열인지 확인하기 위한 변수 
-
-	for (int i = 0; i < n; i++) {
-		//이전 수열의 마지막항과 새로운 수열의 마지막 항이 같으면 중복수열임
-		//그러니깐 같은 k레벨에서 같은 수가 뽑히면 안되기 때문에 k단계에 tmp라는
-		//변수를 두고 i가 0부터 n까지 움직일 때 전에 저장되었던 수를 저장해둠 
-		if (!isused[i] && tmp!=input[i]) {
-			tmp = input[i];
-			arr[k] = input[i]; //저장될 input의 인덱스를 arr에 저장함
-			isused[i] = 1;
-			func(k + 1);
-			isused[i] = 0;
-		}
-	}
-}
+//int arr[10]; //4 2라면 0,0,1,1로 채워둠
+int num[10]; //수를 입력받음 
+int tmp[10]; //뽑았던 수 저장 
 
 int main() {
 	//freopen("input.txt", "r", stdin);
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	int n, m;
 	cin >> n >> m;
 
-	//input을 받음 
-	for (int i = 0; i < n; i++) {
-		cin >> input[i];
-	
-	}
-	//input배열 정렬해줘야함 
-	sort(input, input + n);
+	//입력받고 오름차순으로 정렬해줌
+	for (int i = 0; i < n; i++) cin >> num[i];
+	sort(num, num + n);
 
-	func(0);
+	//순열 만들기
+	do {
+		bool flag = 0;
+		for (int i = 0; i < m; i++) {
+			if (num[i] != tmp[i]) {
+				flag = 1;
+				break;
+			}
+		}
+		if (!flag) continue;
+
+		for (int i = 0; i < m; i++) {
+			cout << num[i] << " ";
+			tmp[i] = num[i];
+		}
+		cout << '\n';
+	} while (next_permutation(num,num+n));
 
 	return 0;
 }
