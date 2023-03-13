0c76719d53412977607f6b5acc1b39e562db229b
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-03-13 19:44:33 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -2,35 +2,8 @@
 #include <algorithm>
 using namespace std;
 
-int input[15]; //k개의 수를 뽑을 배열 
-int arr[8]; //k개 중 6개가 저장될 배열, arr배열에 input인덱스를 저장함
-bool isused[15]; //그 수가 사용되었는지 검사하는 배열
-int k;
-
-void func(int cur) {
-	if (cur == 6) { //6개를 다 뽑았다면 출력해줌
-		for (int i = 0; i < 6; i++) {
-			cout << input[arr[i]] << " ";
-		}
-		cout << '\n';
-
-		return;
-	}
-
-	//cur-1에 뽑힌 수보다 큰 수만 뽑고자 하므로 
-	int st = 0;
-	if (cur != 0) st = arr[cur - 1] + 1;
-
-	for (int i = st; i < k; i++) { //input이 사용되었는지를 하나씩 확인하고 뽑음 
-		if (!isused[i]) {
-			arr[cur] = i; //저장할 input 인덱스 저장
-			isused[i] = 1;
-			func(cur + 1);
-			isused[i] = 0;
-		}
-	}
-
-}
+int arr[15]; //k개 뽑힌 수들 중에서 6개만 뽑을 것임 
+bool visited[15]; //방문 여부 체크 
 
 int main() {
 	//freopen("input.txt", "r", stdin);
@@ -38,20 +11,28 @@ int main() {
 	cin.tie(0);
 
 	while (1) {
-		cin >> k;
-
+		int k; cin >> k;
 		if (k == 0) break;
 
-		fill(input, input + 15, 0);
-		fill(arr, arr + 8, 0);
-		fill(isused, isused + 15, 0);
-
 		for (int i = 0; i < k; i++) {
-			cin >> input[i];
+			cin >> arr[i];
 		}
+		
+		//앞에 6개만 뽑히는 것이므로 값을 0으로 해줌 
+		int choice[15];
+		for (int i = 0; i < 15; i++) choice[i] = 1;
+		for (int i = 0; i < 6; i++) choice[i] = 0;
 
-		func(0);
+		//while문 안에 인자가 arr가 아니라 choice라는 것을 주의하자!
+		do {
+			for (int i = 0; i < k; i++) {
+				if (choice[i] == 0) cout << arr[i] << " ";
+			}
+			cout << '\n';
+		} while (next_permutation(choice, choice + k));
+
 		cout << '\n';
 	}
+	
 	return 0;
 }
