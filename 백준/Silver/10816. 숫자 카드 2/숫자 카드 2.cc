be6563b0ca20ba0f91157af6de862ab9ee6d499c
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-04-17 04:19:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 316 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int a[500005];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n;
+    for(int i=0; i<n; i++) cin>>a[i];
+    sort(a,a+n);
+
+    cin>>m;
+    while(m--){
+        int x;
+        cin>>x;
+        cout<<upper_bound(a,a+n,x)-lower_bound(a,a+n,x)<<" ";
+    }
+}
