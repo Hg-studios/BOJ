eb1b062886da995f119c01713d8035be9f1d2684
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-05-12 21:16:25 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 35 ms, Memory: 13,584 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.cpp
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 
 int arr[102][102];
@@ -10,26 +11,21 @@ int main(int argc, char** argv)
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
         int n; cin>>n;
-        for(int i=0; i<100; i++){
-        	for(int j=0; j<100; j++){
+        for(int i=0; i<n; i++){
+        	for(int j=0; j<n; j++){
             	cin>>arr[i][j];
             }
         }
+        
         int cnt=0;
-        for(int i=0; i<100; i++){
-        	for(int j=0; j<99; j++){
-                for(int k=j+1; k<100; k++){
-                    //이미 짝이 세어진 N극은 세지 않고 넘어감
-                    if(arr[j][i]==1&&arr[k][i]==-1) break;
-                    //아직 짝이 세어지지 않아서 세어줘야함
-                	if(arr[j][i]==1&&arr[k][i]==2) {
-                        cnt++;
-                        arr[k][i]=-1; //이미셌으니 중복으로 세지 않도록 값을 바꿔줌
-                        break;
-                    }
-                    
+        for(int i=0; i<n; i++){
+            int npole=0;
+        	for(int j=0; j<n; j++){
+                if(arr[j][i]==2&&npole==1){
+                    cnt++;
+                    npole=0;
                 }
-            	
+                else if(arr[j][i]==1) npole=1;
             }
         }
 		cout<<"#"<<test_case<<" "<<cnt<<'\n';
