7e805db2857103078511b41e218e7597156c446b
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2023-08-26 16:35:51 +0900

    [Gold IV] Title: 회전 초밥, Time: 600 ms, Memory: 170776 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,73 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br;
-    static StringTokenizer st;
-    static StringBuilder sb;
-    static int n, d, k, c, ans, cnt;
-    static int[] kind, dishes;
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int n, d, k, c;
+	static int[] arr; //회전초밥 접시 수, 하나씩 볼 것임  
+	static int[] kind; //초밥 가짓 수
+	static int ans, cnt;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken()); 
+		d = Integer.parseInt(st.nextToken()); 
+		k = Integer.parseInt(st.nextToken()); 
+		c = Integer.parseInt(st.nextToken());
+		
+		//초기화
+		arr = new int[n];
+		kind = new int[d+1];
+		
+		//회전초밥 벨트에 놓은 접시 
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		//처음 k개를 봄 
+		for (int i = 0; i < k; i++) {
+			if(kind[arr[i]]==0) ans++; //처음으로 먹는 스시 종류이면 ans를 하나 증가시킴
+			kind[arr[i]]++; //이번 스시 종류에 대한 값 하나 증가시킴
+		}
+		
+		cnt = ans; //한번 k개를 보고 난 후의 값을 cnt에 저장해둠, 쿠폰을 제외한 스시 종류값을 저장 
+		ans = (kind[c]==0) ? cnt+1 : cnt;
+		
+		sliding();
+		
+		System.out.println(ans);
+	}
 
-    public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-
-        //초기화
-        dishes = new int[n];
-        kind = new int[d+1]; //초밥의 종류를 저장
-
-        for (int i = 0; i < n; i++) {
-            dishes[i] = Integer.parseInt(br.readLine());
-        }
-
-        //처음 k개의 초밥을 먹음
-        for (int i = 0; i < k; i++) {
-            if(kind[dishes[i]]==0){
-                ans++;
-            }
-            kind[dishes[i]]++;
-        }
-        //쿠폰에 적힌 초밥이 새로운 종류라면
-        cnt = ans;
-        ans = (kind[c]==0)? cnt+1 : cnt;
-
-        //이제 하나씩 먹을 것임 -> 하나 먹고 하나 빼고 하면서 슬라이딩으로
-        sliding();
-
-        System.out.println(ans);
-    }
-
-    private static void sliding() {
-        int start = k;
-
-        while(true){
-            //슬라이드를 밀면서 전에 먹었던 스시는 빼줌
-            kind[dishes[(start-k)%n]]--;
-            //이전 스시 뺐는데 0이면 종류에서도 하나 빼줌
-            if(kind[dishes[(start-k)%n]]==0) cnt--;
-
-            //그 다음 추가되는 스시 더해줌
-            if(kind[dishes[start%n]] == 0){
-                cnt++;
-            }
-            kind[dishes[start%n]]++;
-
-            //앞스시는 빼고 뒷스시는 넣었으니 이번 차례는 다 본것임
-            //몇 종류의 스시를 먹었는지 확인하기
-            ans = Math.max(ans, (kind[c]==0)? cnt+1 : cnt);
-
-            //그 다음 스시로 넘어감
-            start++;
-            //한 바퀴를 다 돌았다면 끝냄
-            if(start == n-1+k) break;
-        }
-    }
+	private static void sliding() {
+		int start = 1;
+		while(true) {
+			//System.out.println("**"+start);
+			//한칸 밀린 초밥을 빼줌 
+			int minus = start-1;
+			//System.out.println(minus%n);
+			kind[arr[minus%n]]--;
+			if(kind[arr[minus%n]]==0) cnt--;
+			
+			//한칸 새로 들어온 초밥을 더해줌 
+			int add = start+k-1;
+			//System.out.println(add%n);
+			if(kind[arr[add%n]]==0) cnt++;
+			kind[arr[add%n]]++;
+			
+			//쿠폰 적용 되는지 체크
+			//cnt = (kind[c]==0)? cnt+1 : cnt;
+			//이번 차례에서 구한 답으로 업데이트해줌
+			ans = Math.max(ans, (kind[c]==0)? cnt+1 : cnt);
+			
+			start++; //다음 k개 보러 감 
+			if(start==n) break; //다 봤으면 끝냄 
+		}
+	}
 }
