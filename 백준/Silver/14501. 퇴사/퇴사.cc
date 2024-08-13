d1b43ea28de5e77ffc7f157cee3e546fb5430eb5
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2024-08-13 18:48:08 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,26 +1,36 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int d[20]; //d[i]=k : i번째 일에 상담을 시작했을 때 얻을 수 있는 최대 수익
-int t[20]; //상담일 수
-int p[20]; //상담액
+#define MAX 16
+int n;
+int t[MAX]; // 상담 걸리는 기간
+int p[MAX]; // 받을 수 있는 금액 
+int dp[MAX];
 
-int main(){
-    int n; cin>>n;
-
-    for(int i=1; i<=n; i++){
-        cin>>t[i]>>p[i];
-    }
-
-    for(int i=n; i>=1; i--){
-        //i번째 일에 상담을 할 수 있는 경우
-        if(i+t[i]-1<=n){
-            //상담을 할 때와 안할 때를 비교하여 큰 값을 넣어줌
-            d[i] = max(d[i+t[i]]+p[i], d[i+1]);
+void solve() {
+    int max_pay = 0;
+    for(int i=0; i<=n; i++) {
+        dp[i] = max(dp[i], max_pay);
+        
+        if(t[i]+i<=n) {
+            dp[t[i]+i] = max(dp[t[i]+i], p[i]+dp[i]);
         }
-        //i번째 일에 상담을 할 수 없는 경우
-        else d[i]=d[i+1];
+        
+        max_pay = max(max_pay, dp[i]);
     }
-    cout<<*max_element(d+1, d+n+1); //1-indexed이므로!!
+    
+    cout<<max_pay;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    cin>>n;
+    for(int i=0; i<n; i++) cin>>t[i]>>p[i];
+    
+    solve();
+    
+    return 0;
 }
