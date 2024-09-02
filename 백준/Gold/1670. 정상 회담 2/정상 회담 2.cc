f779845abb96d33a86e8100a60294f8f528ee015
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2024-09-02 11:57:21 +0900

    [Gold III] Title: 정상 회담 2, Time: 48 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/1670. 정상 회담 2/정상 회담 2.cc
+++ 백준/Gold/1670. 정상 회담 2/정상 회담 2.cc
@@ -1,21 +1,32 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
+
 using ll = long long;
 #define MOD 987654321LL
- 
-ll d[5001] = { 1 };
-int n;
-int main() {
-	cin >> n;
-	n /= 2;
-	for (int i = 1; i < n + 1; i++) {
-		for (int j = 0; j < i; j++) {
-			d[i] += (d[j] % MOD) * (d[i - 1 - j] % MOD) % MOD;
-			d[i] %= MOD;
-		}
-		d[i] %= MOD;
-	}
- 
-	cout << d[n];
+
+ll dp[5002];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n;
+    cin>>n;
+    
+    n/=2;
+    
+    dp[0]=1;
+    dp[1]=1;
+    
+    for(int i=2; i<=n; i++) {
+        for(int j=0; j<i; j++) {
+            dp[i] += ((dp[j]%MOD) * (dp[i-j-1]%MOD)) % MOD;
+            dp[i] %= MOD;
+        }
+    }
+    
+    cout<<dp[n];
+    
+    return 0;
 }
