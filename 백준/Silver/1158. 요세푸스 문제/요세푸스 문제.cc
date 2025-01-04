fd1cd67a6ce839da61c189cef5971459f154dda5
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-01-04 19:58:11 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,30 +1,30 @@
 #include <iostream>
-#include <queue>
-#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
-  int n,m;
-  cin >> n;
-  cin >> m;
-  
-  queue<int> q;
-  
-  for(int i=0; i<n; i++){
-    q.push(i+1);  
-  }
-
-  cout << "<";
-
-  for(int i=0; i<n-1; i++){
-    for(int j=0; j<m-1; j++){
-      q.push(q.front());
-      q.pop();
+int main()
+{
+    int n, k; 
+    cin >> n >> k;
+    vector<int> v(n);
+    
+    for (int i = 0; i < n; i++) {
+        v[i] = i + 1; // 1부터 N까지 초기화
     }
-    cout << q.front() << ", ";
-    q.pop();
-  }
-  cout << q.front() << ">" << '\n';
-  
-  return 0;
+    
+    int index = 0; // 현재 인덱스
+    cout << "<";
+    
+    while (!v.empty()) {
+        index = (index + k - 1) % v.size(); // k-1 만큼 이동 후 현재 남은 크기에서 모듈러 연산
+        cout << v[index]; // 제거되는 사람 출력
+        v.erase(v.begin() + index); // 제거
+        
+        if (!v.empty()) {
+            cout << ", "; // 아직 남은 사람이 있다면 쉼표 추가
+        }
+    }
+    
+    cout << ">" << endl;
+    return 0;
 }
