c45976b9f17a40e9b91369bc1783499e75725718
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-02-01 21:06:27 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -9,15 +9,18 @@ int main()
     vector<double> v(n);
     for(int i=0; i<n; i++) cin>>v[i];
     
-    double ans=0;
-    for(int i=0; i<n; i++) {
-        double result = 1;
-        for(int j=i; j<n; j++) {
-            result *= v[j];
-            ans = max(ans, result);
-        }
-    }
+    vector<double> dp(n);
+    dp[0]=v[0]; // dp[a] : a를 포함할 때 연속된 곱의 최대값
     
+    double ans=dp[0];
+    for(int i=1; i<n; i++) {
+        // 나를 포함할 때 나부터 다시 시작하거나 
+        // dp[i-1] 은 i-1은 포함하니깐 전의 수부터 시작되어왔거나
+        dp[i] = max(v[i], dp[i-1]*v[i]);
+        ans = max(ans, dp[i]);
+        // cout<<dp[i]<<" ";
+    }
+    // cout<<'\n';
     printf("%.3lf", ans);
     
     return 0;
