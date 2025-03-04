cd33a105421109a751074b80ddb6a0b4a5eec97d
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-03-04 21:29:09 +0900

    [level 1] Title: 추억 점수, Time: 2.08 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    map<string, int> m;
+    
+    for(int i=0; i<name.size(); i++) m[name[i]] = yearning[i];
+    
+    for(auto pho : photo) {
+        int sum=0;
+        for(auto p : pho) {
+            sum+=m[p];
+        }
+        answer.push_back(sum);
+    }
+    
+    return answer;
+}
