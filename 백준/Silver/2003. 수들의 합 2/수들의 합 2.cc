5a3747a087973154c280bcfb920ce8b135b02f08
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-03-05 15:41:59 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n,m; cin>>n>>m;
+    
+    vector<int> v(n);
+    for(int i=0; i<n; i++) cin>>v[i];
+    
+    int en=0, sum=v[0], ans=0;
+    
+    for(int st=0; st<n; st++) {
+        // 현재 st와 en까지의 범위의 합이 m보다 작다면 
+        // en을 계속 증가시켜줌
+        while(sum<m && en<n-1) {
+            en++;
+            sum+=v[en];
+        }
+        
+        if(sum==m) ans++;
+        
+        sum-=v[st];
+    }
+    
+    cout<<ans;
+    
+    return 0;
+}
