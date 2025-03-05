deb89bcf1a93456fa918ed84d97e7e6377258456
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-03-05 14:42:18 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -2,33 +2,31 @@
 #include <algorithm>
 using namespace std;
 
-int n,s;
+int n, s;
 int arr[100005];
-int mn=0x7fffffff;
+int mn = 0x7fffffff;
 
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin>>n>>s;
-    for(int i=0; i<n; i++) cin>>arr[i];
+    cin >> n >> s;
+    for (int i = 0; i < n; i++) cin >> arr[i];
 
-    int en=0;
-    int tot=arr[0];
+    int st = 0, en = 0, tot = 0;
+    
+    while (en < n) {
+        tot += arr[en]; // 부분합에 en 추가
 
-    for(int st=0; st<n; st++){
-        //현재 st에 대해 합이 s보다 작아지는 en을 아직
-        //찾지 못한 상황이므로 찾을 때까지 en을 오른쪽으로 이동함
-        while(en<n && tot<s){
-            en++;
-            if(en!=n) tot+=arr[en];
+        while (tot >= s) { // 조건 만족하면 최소 길이 갱신 및 st 이동
+            mn = min(mn, en - st + 1);
+            tot -= arr[st];
+            st++;
         }
-        if(en==n) break;
-        //tot이 s보다 큰 상황이 되면 st을 오른쪽으로 한칸 옮겨줌
-        mn=min(mn, en-st+1);
-        tot-=arr[st];
+
+        en++; // en을 이동
     }
 
-    if(mn==0x7fffffff) mn=0;
-    cout<<mn;
+    cout << (mn == 0x7fffffff ? 0 : mn);
+    return 0;
 }
