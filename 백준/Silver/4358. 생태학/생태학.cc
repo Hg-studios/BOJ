337fee9a6803f0b5e0509c90df3ffd6e599087a7
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-05-01 03:38:04 +0900

    [Silver II] Title: 생태학, Time: 160 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    map<string, int> m; 
+    
+    // map은 기본적으로 사전순으로 정렬됨
+    
+    int total=0;
+    while(1) {
+        string s;
+        //개행을 기준으로 한줄씩 받아야함
+        getline(cin, s);
+        
+        if(s=="") break;
+        
+        m[s]++;
+        total++;
+    }
+    
+    cout<<fixed;
+    cout.precision(4);
+    
+    for(auto& cur : m) {
+        cout<<cur.first<<" "<<(cur.second/(double)total)*(double)100<<'\n';
+    }
+    
+    
+    return 0;
+}
