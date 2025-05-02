2bd5867e2242b259e75f6a8b55702451986c353a
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-05-02 12:07:26 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -15,15 +15,13 @@ int main()
     int cnt=0;
     for(int i=0; i<s.size(); i++) {
         if(s[i]=='.') {
-            // ans+='.';
-        
-            // 현재 cnt값으로 그리디하게 문자열 구해주기 
-            // cnt가 0이면 그 전에도 . 이었다는 말이므로 출력하는 부분을 건너뜀 
+            // cnt가 0이면 그 전에도 . 이었다는 말이므로 아래 출력하는 부분을 건너뜀 
             if(cnt==0) {
                 ans+='.';
                 continue; 
             }
             
+            // 현재 cnt값으로 그리디하게 문자열 구해주기 
             //일단 AAAA를 넣고 나머지 자리에 BB 넣기
             int four = cnt/4;
             for(int i=0; i<four; i++) ans+="AAAA";
@@ -31,7 +29,8 @@ int main()
             int two = (cnt - 4*four)/2;
             for(int i=0; i<two; i++) ans+="BB";
             
-            if(cnt-4*four-2*two!=0) {
+            int remain = cnt-4*four-2*two;
+            if(remain!=0) {
                 cout<<"-1";
                 return 0;
             }
@@ -43,26 +42,22 @@ int main()
         }
     }
     
-    // cout<<cnt;
     if(cnt!=0) {
         //일단 AAAA를 넣고 나머지 자리에 BB 넣기
         int four = cnt/4;
         for(int i=0; i<four; i++) ans+="AAAA";
-        // cout<<four<<" ";
         
         int two = (cnt - 4*four)/2;
         for(int i=0; i<two; i++) ans+="BB";
-        // cout<<two<<" ";
         
         int remain = cnt-4*four-2*two;
-        // cout<<remain<<" ";
         if(remain!=0) {
             cout<<"-1";
             return 0;
         }
     }
-    cout<<ans;
     
+    cout<<ans;
     
     return 0;
 }
