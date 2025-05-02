34aa12b60ef9b3338536c6457d4be78fa8304775
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-05-02 12:24:50 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -14,50 +14,26 @@ int main()
     
     int cnt=0;
     for(int i=0; i<s.size(); i++) {
-        if(s[i]=='.') {
-            // cnt가 0이면 그 전에도 . 이었다는 말이므로 아래 출력하는 부분을 건너뜀 
-            if(cnt==0) {
-                ans+='.';
-                continue; 
-            }
-            
-            // 현재 cnt값으로 그리디하게 문자열 구해주기 
-            //일단 AAAA를 넣고 나머지 자리에 BB 넣기
-            int four = cnt/4;
-            for(int i=0; i<four; i++) ans+="AAAA";
-            
-            int two = (cnt - 4*four)/2;
-            for(int i=0; i<two; i++) ans+="BB";
-            
-            int remain = cnt-4*four-2*two;
-            if(remain!=0) {
-                cout<<"-1";
-                return 0;
-            }
+        if(s[i]=='X') cnt++;
+        else if(s[i]=='.') {
             ans+='.';
+            
+            if(cnt%2!=0) break; // .을 만나서 X개수만큼 출력해야 되는데 홀수 출력할 수 없음
+            else cnt=0;
+        }
+        
+        if(cnt==2 && s[i+1]!='X') {
+            ans+="BB";
             cnt=0;
         }
-        else {
-            cnt++;
+        if(cnt==4) {
+            ans+="AAAA";
+            cnt=0;
         }
     }
     
-    if(cnt!=0) {
-        //일단 AAAA를 넣고 나머지 자리에 BB 넣기
-        int four = cnt/4;
-        for(int i=0; i<four; i++) ans+="AAAA";
-        
-        int two = (cnt - 4*four)/2;
-        for(int i=0; i<two; i++) ans+="BB";
-        
-        int remain = cnt-4*four-2*two;
-        if(remain!=0) {
-            cout<<"-1";
-            return 0;
-        }
-    }
-    
-    cout<<ans;
+    if(cnt%2==1) cout<<-1;
+    else cout<<ans;
     
     return 0;
 }
