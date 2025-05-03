63707ea155ac3edd05f669f6d053d847c13f635d
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-05-03 21:57:30 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,24 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n;
-int a[1002];
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin>>n;
-    for(int i=1; i<=n; i++) cin>>a[i];
-
-    sort(a+1,a+n+1);
-    int sum=0;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            sum +=a[j];
-        }
+int main()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++) cin>>v[i];
+    
+    sort(v.begin(), v.end());
+    
+    int ans=0;
+    int delay=0;
+    for(int i=0; i<v.size(); i++) {
+        ans+=delay;
+        delay+=v[i];
+        
+        // cout<<ans<<" "<<delay<<'\n';
     }
-
-    cout<<sum;
+    
+    cout<<ans+delay;
+    
+    return 0;
 }
