f05e0475f53fb5e3a80116a3d42377aa42be5a28
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-05-09 00:52:01 +0900

    [Gold IV] Title: 합이 0, Time: 1812 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -3,27 +3,33 @@
 #include <algorithm>
 using namespace std;
 
-int N;
-
-int main(){
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    cin >> N;
-    vector<int> v(N);
-    for(int i = 0; i < N; i++) cin >> v[i];
-
-    sort(v.begin(),v.end()); // 오름차순 정렬
-    long long ans = 0; // 경우의 수가 int를 넘을 수 있음
-
-    // 이중 반복으로 2개의 값을 고름
-    for(int i = 0; i < v.size()-1; i++){
-        for(int j = i+1; j < v.size(); j++){
-            // 구해야할 나머지 1개의 값을 구함
-            long long cnt = v[i]+v[j];
-            // 개수 찾는 과정
-            int idx1 = lower_bound(v.begin()+j+1,v.end(),-cnt)-v.begin();
-            int idx2 = upper_bound(v.begin()+j+1,v.end(),-cnt)-v.begin();
-            ans += idx2 - idx1; // 구한 개수를 더함
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n;  cin>>n;
+    
+    // 원래는 n^3이지만 두 개의 합(sum)을 구해서 
+    // -sum이 있는지 확인해보면 n^2logn 이므로 4초 안에 가능함
+    
+    vector<int> v(n);
+    for(int i=0; i<n; i++) cin>>v[i];
+    
+    sort(v.begin(), v.end());
+    
+    ll ans=0;
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            int sum = v[i] + v[j];
+            
+            // cout<<v[i]<<" "<<v[j]<<" "<<-sum<<'\n';
+            ans += (upper_bound(v.begin()+j+1, v.end(), -sum) - lower_bound(v.begin()+j+1, v.end(), -sum));
         }
     }
-    cout << ans;
+    
+    cout<<ans;
+    
+    return 0;
 }
