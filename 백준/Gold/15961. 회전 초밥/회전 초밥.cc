ce846f8dbee36845d38ccd171f496aeb218a3266
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-05-09 15:22:42 +0900

    [Gold IV] Title: 회전 초밥, Time: 344 ms, Memory: 13876 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.cc
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <limits.h>
-#include <map>
+#include <unordered_map>
 using namespace std;
 #define ll long long
 
@@ -15,7 +15,7 @@ int main()
     vector<int> v(n);
     for(int i=0; i<n; i++) cin>>v[i];
     
-    map<int, int> m; //초밥 종류, 개수
+    unordered_map<int, int> m; //초밥 종류, 개수
     
     int kind=0;
     
@@ -32,22 +32,24 @@ int main()
     int mx = -1;
     // k번째부터 최대값을 구해주기
     // i가 k까지인줄 알았으나 초밥판은 순환하기 때문에 총 n번을 돌아야 함
-    for(int i=k-1; i<=n+k-2; i++) {
-        // i를 포함해서 총 k개가 포함된 상태
-        int tmp = i%n;
-        if(m[v[tmp]]==0) kind++;
-        m[v[tmp]]++;
+    // 매번 이런 슬라이딩 윈도우는 인덱스때문에 시간이 너무 많이 걸렸는데 그냥 for문의 인덱스와 내부에서 쓰는 인덱스를 따로 두자!
+    
+    int start=0, end=k-1; //둘은 인덱스를 의미
+    for(int i=0; i<n; i++) { //n번을 돌자 -> 그냥 n번을 카운팅하기 위해서만 사용함 
+        // end에 해당하는 초밥은 추가해줌 
+        int tmp = v[end%n];
+        if(m[tmp]==0) kind++;
+        m[tmp]++;
         
-        // cout<<kind<<'\n';
-        // for(auto x : m) {
-        //     cout<<x.first<<" "<<x.second<<'\n';
-        // }
-        // cout<<'\n';
         if(kind>mx) mx = kind;
         
-        tmp = (i-k+1)%n;
-        m[v[tmp]]--;
-        if(m[v[tmp]]==0) kind--;
+        //start에 해당하는 초밥은 삭제함
+        tmp = v[start%n];
+        m[tmp]--;
+        if(m[tmp]==0) kind--;
+        
+        start++;
+        end++;
     }
     
     cout<<mx;
