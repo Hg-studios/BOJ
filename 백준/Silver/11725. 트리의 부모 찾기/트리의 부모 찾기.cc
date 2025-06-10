27d71a5bc3cf251d79930675a60c98c8b9bdfae9
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-06-10 20:15:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,61 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 using namespace std;
 
-#define MAX 100001
+vector<vector<int>> v;
 
-int n;
-int ans[MAX];
-bool visited[MAX];
-vector<int> graph[MAX];
-
-void input() {
-    cin>>n;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr); cout.tie(nullptr);
     
-    for(int i=2; i<=n; i++) {
-        int x, y;
-        cin>>x>>y;
-        
-        graph[x].push_back(y);
-        graph[y].push_back(x);
-        
+    int n; cin>>n;
+    
+    vector<vector<int>> v(n+1);
+    
+    for(int i=0; i<n-1; i++) {
+        int x, y; cin>>x>>y;
+        v[x].push_back(y);
+        v[y].push_back(x);
     }
-}
-
-void solve() {
-    queue<int> q;
     
-    visited[1] = true;
+    queue<int> q;
+    vector<bool> visited(n+1);
+    
+    visited[1]=true;
     q.push(1);
+    
+    vector<int> parent(n+1);
     
     while(!q.empty()) {
         int cur = q.front();
         q.pop();
         
-        for(int next : graph[cur]) {
+        for(int next : v[cur]) {
             if(visited[next]) continue;
             
-            ans[next] = cur;
-            
-            visited[next]=1;
+            parent[next] = cur;
+            visited[next] = true;
             q.push(next);
         }
     }
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    input();
-    solve();
     
     for(int i=2; i<=n; i++) {
-        cout<<ans[i]<<'\n';
+        cout<<parent[i]<<'\n';
     }
+    
     return 0;
 }
-
