b879fd2cda407a5ec608e42da886be00647ad342
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-06-11 16:04:42 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 112 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -14,8 +14,6 @@ int main()
         cin>>d[i];
     }
     
-    // 로봇의 위치를 저장할 공간 -> 삽입 삭제가 쉬워야 함 
-    
     // 0 ~ 2n-1칸이 있는 상황
     // 컨베이터벨트 위에 벨트가 돌아가는 상황이고 벨트 위에 로봇을 올리는 것
     
@@ -34,32 +32,17 @@ int main()
         }
         d[0] = tmp;
         
-        // cout<<"[내구도 한 칸 이동]\n";
-        // for(int x : d) cout<<x<<" ";
-        // cout<<'\n';
-        
-        // cout<<"[로봇이동 전]\n";
-        // for(int x : robots) cout<<x<<" ";
-        // cout<<'\n';
-        
         // 1-2. 로봇 한 칸 이동
         for(int i=n-1; i>0; i--) {
             robots[i] = robots[i-1];
             robots[i-1] = false;
         }
         
-        // cout<<"[로봇이동 후]\n";
-        // for(int x : robots) cout<<x<<" ";
-        // cout<<'\n';
-        
         // 1-3. 이동 후 내리는 위치에 있는 로봇 삭제
         robots[n-1] = false;
         
-        // cout<<"[로봇이동 후 마지막 로봇 처리]\n";
-        // for(int x : robots) cout<<x<<" ";
-        // cout<<'\n';
-        
         // 2. 로봇만 이동가능하다면 이동
+        
         // 2-1. 가장 오래된 로봇부터 한 칸씩 옮겨줌줌
         // & 이동했는데 내리는 칸이면 삭제
         // & 이동한 칸의 내구도 1 감소
@@ -74,11 +57,9 @@ int main()
                 d[i+1]--; // 이동하는 칸의 내구도 1 감소 
             }
         }
+        
         // 2-2. 마지막 칸에 온 로봇은 삭제
         robots[n-1] = false;
-        // cout<<"[로봇이동 후3]\n";
-        // for(int x : robots) cout<<x<<" ";
-        // cout<<'\n';
         
         // 3. 올리는 칸에 로봇 올리기
         
@@ -96,20 +77,6 @@ int main()
             if(!d[i]) cnt++;
         }
         if(cnt>=k) break;
-        
-        // cout<<"[최종] "<<turn<<'\n';
-        // for(int x : robots) cout<<x<<" ";
-        // cout<<'\n';
-        
-        // for(int x : d) cout<<x<<" ";
-        // cout<<'\n';
-        // cout<<'\n';
-        
-        
-        
-        
-        // if(cnt>=3) break;
-        // if(turn>=24) break;
     }
     
     cout<<turn;
