62d192704347e1f373923c75470510e84a48cede
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-06-13 15:35:26 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 16 ms, Memory: 2672 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 #include <string>
 #include <map>
@@ -10,41 +18,47 @@ int main()
     cin.tie(nullptr); cout.tie(nullptr);
     
     int n,m; cin>>n>>m;
-    string tmp;
-    for(int i=0; i<21; i++) tmp+='0';
-    vector<string> v(n+1, tmp);
+    
+    vector<int> v(n+1, 0);
     
     for(int i=0; i<m; i++) {
         int command; cin>>command;
         
         if(command==1) {
             int x, y; cin>>x>>y;
-            v[x][y] = '1';
+            // v[x][y] = '1';
+            v[x] |= (1<<y);
         }
         else if(command==2) {
             int x, y; cin>>x>>y;
-            v[x][y] = '0';
+            // v[x][y] = '0';
+            v[x] &= ~(1<<y);
         }
         else if(command==3) {
             int x; cin>>x;
-            for(int j=20; j>0; j--) v[x][j] = v[x][j-1];
-            v[x][1] = '0';
+            // for(int j=20; j>0; j--) v[x][j] = v[x][j-1];
+            // v[x][1] = '0';
+            
+            v[x] = v[x]<<1;
+            v[x] &= ((1<<21)-1);
         }
         else {
             int x; cin>>x;
-            for(int j=1; j<20; j++) v[x][j] = v[x][j+1];
-            v[x][20] = '0';
+            // for(int j=1; j<20; j++) v[x][j] = v[x][j+1];
+            // v[x][20] = '0';
+            v[x] = v[x]>>1;
+            v[x] &= ~1; //0번째 위치는 승객이 넘어왔어도 0으로 만들어줘야 함 
+            //ex. (20) 01001111 (1)
+            
+            
         }
     }
     
-    map<string, int> mp;
+    vector<bool> vis(1<<21, false);
     int cnt=0;
-    
     for(int i=1; i<=n; i++) {
-        // cout<<v[i]<<" ";
-        
-        if(mp[v[i]]==0) cnt++;
-        mp[v[i]]++;
+        if(vis[v[i]]==false) cnt++;
+        vis[v[i]] = true;
     }
     
     cout<<cnt;
