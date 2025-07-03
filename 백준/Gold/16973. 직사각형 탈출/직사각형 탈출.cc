4b4c83d0c870a1c322c35c8bc17ec22067c0dca0
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-07-03 15:44:14 +0900

    [Gold IV] Title: 직사각형 탈출, Time: 84 ms, Memory: 13920 KB -BaekjoonHub

--- 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.cc
+++ 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.cc
@@ -1,130 +1,79 @@
-#include<iostream>
-#include<queue>
- 
-#define endl "\n"
-#define MAX 1000
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
- 
-int N, M, H, W, Sx, Sy, Ex, Ey;
-int MAP[MAX][MAX];
-bool Visit[MAX][MAX];
- 
-int dx[] = { 0, 0, 1, -1 };
-int dy[] = { 1, -1, 0, 0 };
- 
-void Input()
+
+#define X first
+#define Y second
+
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+
+int main()
 {
-    cin >> N >> M;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cin >> MAP[i][j];
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n,m; cin>>n>>m;
+    
+    vector<vector<int>> v(n+1, vector<int> (m+1));
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            cin>>v[i][j];
         }
     }
-    cin >> H >> W >> Sx >> Sy >> Ex >> Ey;
-    Sx--; Sy--; Ex--; Ey--;
-}
- 
-bool Can_Move(int x, int y, int dir)
-{
-    if (dir == 0)
-    {
-        int ny = y + W - 1;
-        if (ny >= M) return false;
- 
-        for (int i = x; i < x + H; i++)
-        {
-            if (MAP[i][ny] == 1) return false;
+    
+    int h,w; cin>>h>>w;
+    int sr, sc, fr, fc; cin>>sr>>sc>>fr>>fc;
+    
+    // 누적합을 위한 배열 sum
+    // (1,1) 부터 (x,y) 까지의 누적합
+    vector<vector<int>> sum(n+1, vector<int> (m+1,0));
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            sum[i][j] = v[i][j] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
         }
     }
-    else if (dir == 1)
-    {
-        for (int i = x; i < x + H; i++)
-        {
-            if (MAP[i][y] == 1) return false;
+    
+    vector<vector<int>> dist(n+1, vector<int> (m+1,-1)); // 방문여부 저장배열이자 거리배열
+    queue<pair<int,int>> q;
+    
+    dist[sr][sc]=0;
+    q.push({sr,sc});
+    
+    while(!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        
+        if(cur.X==fr && cur.Y==fc) {
+            cout<<dist[cur.X][cur.Y];
+            return 0;
+        }
+        
+        for(int dir=0; dir<4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            
+            if(nx<=0 || nx+h-1>n || ny<=0 || ny+w-1>m) continue; // 범위 체크
+            if(dist[nx][ny]!=-1) continue; // 이미 살펴본 좌표라면 탐색 끝
+            
+            // (nx,ny)를 시작점으로 h*w 칸만큼 이동할 수 있는지 확인
+            // 누적합으로 O(1)에 확인해야 시간초과나지 않음 
+            // (nx, ny) ~ (endr, endc) 까지의 누적합을 구해야함 
+            int endr = nx+h-1;
+            int endc = ny+w-1;
+            
+            int s = sum[endr][endc]-sum[nx-1][endc]-sum[endr][ny-1]+sum[nx-1][ny-1];
+            
+            if(s!=0) continue;
+            
+            // cout<<"queue push : "<<nx<<" "<<ny<<'\n';
+            dist[nx][ny] = dist[cur.X][cur.Y]+1;
+            q.push({nx,ny});
         }
     }
-    else if (dir == 2)
-    {
-        int nx = x + H - 1;
-        if (nx >= N) return false;
- 
-        for (int i = y; i < y + W; i++)
-        {
-            if (MAP[nx][i] == 1) return false;
-        }
-    }
-    else if (dir == 3)
-    {
-        for (int i = y; i < y + W; i++)
-        {
-            if (MAP[x][i] == 1) return false;
-        }
-    }
- 
-    return true;
-}
- 
-void BFS(int a, int b)
-{
-    queue<pair<pair<int, int>, int>> Q;
-    Q.push(make_pair(make_pair(a, b), 0));
-    Visit[a][b] = true;
- 
-    while (Q.empty() == 0)
-    {
-        int x = Q.front().first.first;
-        int y = Q.front().first.second;
-        int Cnt = Q.front().second;
-        Q.pop();
- 
-        if (x == Ex && y == Ey)
-        {
-            cout << Cnt << endl;
-            return;
-        }
- 
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
- 
-            if (nx >= 0 && ny >= 0 && nx < N && ny < M)
-            {
-                if (Visit[nx][ny] == false)
-                {
-                    if (Can_Move(nx, ny, i) == true)
-                    {
-                        Visit[nx][ny] = true;
-                        Q.push(make_pair(make_pair(nx, ny), Cnt + 1));
-                    }
-                }
-            }
-        }
-    }
-    cout << -1 << endl;
-}
- 
-void Solution()
-{
-    BFS(Sx, Sy);
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
+    
+    cout<<-1;
+    
     return 0;
 }
