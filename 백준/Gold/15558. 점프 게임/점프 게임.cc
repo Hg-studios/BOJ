19edddb9ba614035dded0e8ff9c75d3a50cbd977
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-07-10 20:47:43 +0900

    [Gold V] Title: 점프 게임, Time: 4 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.cc
+++ 백준/Gold/15558. 점프 게임/점프 게임.cc
@@ -29,13 +29,9 @@ int main()
     
     string s1, s2; cin>>s1>>s2;
     
-    
     // 1-indexed 로 만들어줌
     s1 = "0" + s1;
-    // cout<<s1<<'\n';
-    // cout<<s1[1]<<" "<<s1[n];
     s2 = "0" + s2;
-    // cout<<s1<<" "<<s2<<'\n';
     
     queue<state> q;
     int dx[] = {1,-1,k};
@@ -45,51 +41,34 @@ int main()
     q.push({1,0,1}); // 1칸에 왼쪽 (0)의 1인덱스에서 출발
     
     while(!q.empty()) {
-        auto cur = q.front();
+        auto [curIdx, curPos, curSec] = q.front();
         // cout<<cur.idx<< " "<<cur.pos<<" "<<cur.sec<<'\n';
         q.pop();
         
-        if(cur.idx>=n+1) {
+        if(curIdx>=n+1) {
             cout<<1;
             return 0;
         }
         
         // 지금 위치에서 갈 수 있는 행동을 취함
         for(int dir=0; dir<3; dir++) {
-            int nx = cur.idx + dx[dir];
-            int npos;
+            int nx = curIdx + dx[dir];
+            int npos = (dir<2 ? curPos : 1-curPos);
+            int nsec = curSec + 1;
             
-            if(dir<2) npos = cur.pos; 
-            else npos = 1-cur.pos;
-            
-            if(nx<1) continue; // 범위를 벗어남
             if(nx>=n+1) { // n번 칸보다 더 큰 칸으로 이동한 경우
                 cout<<1;
                 return 0;
             }
-            if(visited[npos][nx]) continue;
-            if(nx<=cur.sec) continue; // nx는 그 다음 초에 가는 것인데 그러면 cur의 sec 칸은 없어지게 됨
-            if(dir==0||dir==1) { // 현재 줄을 그대로 가는 경우 
-                if(cur.pos==0) { // 현재 줄이 왼쪽인 경우 
-                    if(s1[nx]=='0') continue;
-                }
-                else { // 현재 줄이 오른쪽인 경우 
-                    if(s2[nx]=='0') continue;
-                }
-            }
-            else { // 옆의 줄로 이동하는 경우 
-                if(cur.pos==0) { // 현재 줄이 왼쪽인 경우 
-                    if(s2[nx]=='0') continue;
-                }
-                else { // 현재 줄이 오른쪽인 경우 
-                    if(s1[nx]=='0') continue;
-                }
-            }
+            
+            if(nx<1) continue; // 범위를 벗어남
+            if(visited[npos][nx]) continue; // 이미 방문한 적이 있는 경우 탐색 끝
+            if(nx<=curSec) continue; // nx는 그 다음 초에 가는 것인데 그러면 cur의 sec 칸은 없어지게 됨
+            if((npos==0? s1[nx] : s2[nx])=='0') continue;
             
             visited[npos][nx] = true;
             // 그 다음 칸으로 이동할 수 있다고 판단된 경우
-            if(dir==0||dir==1) q.push({nx, cur.pos, cur.sec+1});
-            else q.push({nx, 1-cur.pos, cur.sec+1});
+            q.push({nx, npos, nsec});
         }
     }
     
