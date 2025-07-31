73e55f8e43f38f7cee64e72d37f9fed1d370d94c
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-08-01 02:57:36 +0900

    [Silver III] Title: Four Squares, Time: 8 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -4,27 +4,30 @@
 #include <limits.h>
 using namespace std;
 
-vector<int> dp;
-
-int recur(int n) {
-    if (n == 0) return 0;
-    if (dp[n] != -1) return dp[n];
-
-    int mn = INT_MAX;
-    for (int i = 1; i*i <= n; i++) {
-        mn = min(mn, recur(n - i*i) + 1);
-    }
-
-    dp[n] = mn;
-    return mn;
-}
-
 int main()
 {
-    int n; cin >> n;
-
-    dp.resize(n + 1, -1);
-    cout << recur(n);
-
+    int n; cin>>n;
+    
+    vector<int> dp(n+1, -1); // dp[a]=b : a를 표현할 수 있는 제곱수 합의 최소 개수 b
+    
+    dp[0]=0;
+    dp[1]=1;
+    
+    for(int i=2; i<=n; i++) {
+        int mn = INT_MAX;
+        
+        // i보다 작은 제곱수들을 모두 봄
+        // 그 제곱수의 dp값에다가 +1 하면 현재 dp 값이 나오므로
+        for(int j=1; j*j<=i; j++) {
+            mn = min(mn, dp[i-j*j]+1);
+        }
+        
+        dp[i] = mn;
+    }
+    
+    // dp[a] 는 결국 a/2까지만 보면 됨 . . . 
+    
+    cout<<dp[n];
+    
     return 0;
 }
