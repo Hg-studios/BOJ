149df62da315835b9381d63ed9e85753145913bb
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-08-26 19:25:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.30 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -7,16 +7,14 @@ using namespace std;
 
 string to_binary (int n) {
     string tmp="";
+    
     while(1) {
-        
         tmp = to_string(n%2) + tmp;
         n/=2;
         
-        if(n==0) {
-            break;
-        }
+        if(n==0) break;
     }
-    // cout<<tmp;
+    
     return tmp;
 }
 
@@ -33,15 +31,15 @@ vector<int> solution(string s) {
         
         int tmp = 0;
         
-        for(auto c : cur) {
+        for(auto c : cur) { // 0의 개수 세기
             if(c=='0') tmp++;
         }
         
-        cnt+=tmp;
+        cnt+=tmp; // 이번 턴에서의 0의 개수 저장
         
-        cur = to_binary(cur.size() - tmp);
+        cur = to_binary(cur.size() - tmp); // 1의 개수를 이진 변환하기
         
-        if(cur=="1") break;
+        if(cur=="1") break; // 1이 되면 탈출
     }
     
     answer.push_back(turn);
