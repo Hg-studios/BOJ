6b538591d354a97f00919e36745eb0221184d0c0
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-08-31 01:03:29 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int solution(int n)
 {
     int ans = 0;
     
-    while(n>0) {
-        if(n%2==0) n/=2;
-        else {
-            n-=1;
+    // n부터 시작해서 0을 만들기!!
+    // 무조건 2로 나누는 게 이득임
+    
+    while(1) {
+        if(n%2==1) {
             ans++;
+            n-=1;
         }
+        else {
+            n/=2;
+        }
+        
+        if(n==0) break;
     }
+    
+
     return ans;
 }
