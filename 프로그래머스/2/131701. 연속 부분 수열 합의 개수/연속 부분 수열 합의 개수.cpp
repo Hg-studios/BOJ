65f47102bf488509b27753af8a4cfde6109a4b54
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-04 02:04:06 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1278.56 ms, Memory: 23 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -7,18 +7,15 @@ int solution(vector<int> elements) {
     int answer = 0;
     
     set<int> s;
+    int n = elements.size();
     
-    // 1부터 elements.size()만큼 
-    for(int size=1; size<=elements.size(); size++) {
-        // 각 size별로 시작점은 start임
-        // 실제 더하기를 시작할 인덱스
-        for(int start=0; start<elements.size(); start++) {
-            // 시작하는 인덱스부터 size만큼 더하기 
-            int sum = 0;
-            for(int cnt=start; cnt<start+size; cnt++) {
-                sum += elements[cnt%elements.size()];
+    for(int i=1; i<=n; i++) { // 개수 
+        for(int j=0; j<n; j++) { // 시작점 
+            int tmp = 0;
+            for(int k=j; k<j+i; k++) { // 시작점에서 개수만큼
+                tmp += elements[k%n];
             }
-            s.insert(sum);
+            s.insert(tmp);
         }
     }
     
