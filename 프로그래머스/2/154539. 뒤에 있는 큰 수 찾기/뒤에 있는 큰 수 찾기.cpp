acb4c946dbc65cdf4f879fed7e5b3fc523333f96
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-12 09:50:02 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 416.12 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -1,23 +1,25 @@
 #include <string>
 #include <vector>
-
+#include <stack>
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    int n = (int)numbers.size();
-    vector<int> answer(n, -1);
-    // std::stack<int>보다 vector<int>를 스택처럼 쓰면 약간 더 빠른 경우가 많음
-    vector<int> st;
-    st.reserve(n);
-
-    for (int i = 0; i < n; ++i) {
-        // 현재 값이 스택 상단(아직 미해결)의 값보다 크면 답을 채운다
-        while (!st.empty() && numbers[st.back()] < numbers[i]) {
-            answer[st.back()] = numbers[i];
-            st.pop_back();
+    int n = numbers.size();
+    vector<int> ans(n, -1);
+    
+    // stack에 저장되어있는 인덱스 값은 이미 지나온 값임
+    // 즉, 현재 값과 스택에 저장되어 있는 값을 비교하는 것은 과거값과 비교하는 것이므로
+    // stack의 top과 비교해서 나보다 작은 수가 있을 때까지 나의 값으로 ans를 채움
+    
+    stack<int> s; // 인덱스 저장
+    for(int i=0; i<n; i++) {
+        while(!s.empty() && numbers[s.top()] < numbers[i]) {
+            ans[s.top()] = numbers[i];
+            s.pop();
         }
-        st.push_back(i);
+        s.push(i);
     }
-    // 남은 인덱스는 기본값 -1 유지
-    return answer;
+    
+    return ans;
+
 }
