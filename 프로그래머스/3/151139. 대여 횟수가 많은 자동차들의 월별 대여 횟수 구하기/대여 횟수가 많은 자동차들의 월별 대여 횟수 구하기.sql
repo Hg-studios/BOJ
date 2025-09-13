b05812f9e1cc2e62d78b2caefa59fba5b0bbf000
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-14 02:09:12 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,12 +1,18 @@
--- 코드를 입력하세요
-SELECT MONTH(START_DATE) AS MONTH, CAR_ID, COUNT(*) RECORDS
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE CAR_ID IN (SELECT CAR_ID
-                FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                WHERE MONTH(START_DATE) BETWEEN 8 AND 10
-                GROUP BY CAR_ID
-                HAVING COUNT(*)>=5)
-AND MONTH(START_DATE) BETWEEN 8 AND 10
-
-GROUP BY MONTH(START_DATE), CAR_ID
-ORDER BY MONTH, CAR_ID DESC;
+select month(START_DATE) MONTH, CAR_ID, count(*) RECORDS
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where CAR_ID in (
+    -- 2022년 8월부터 2022년 10월까지 총 대여 횟수가 5회 이상인 자동차 뽑기
+    select CAR_ID
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    where date_format(START_DATE, '%Y-%m') >= '2022-08'
+    and date_format(START_DATE, '%Y-%m') <= '2022-10'
+    group by CAR_ID
+    having count(*)>=5
+)
+-- 위에서 적용한 날짜 조건은 해당 자동차를 찾기 위함이었고, 해당 sql은 car_id만 반환하게 됨
+-- 따라서 이 car_id에 해당하는 자동차라고 하더라도 8월~10월 사이가 아닐 수 있으므로
+-- 다시 한 번 날짜에 대한 조건을 넣어서 걸러줘야 함
+and date_format(START_DATE, '%Y-%m') >= '2022-08'
+and date_format(START_DATE, '%Y-%m') <= '2022-10'
+group by month(START_DATE), CAR_ID
+order by MONTH, CAR_ID desc
