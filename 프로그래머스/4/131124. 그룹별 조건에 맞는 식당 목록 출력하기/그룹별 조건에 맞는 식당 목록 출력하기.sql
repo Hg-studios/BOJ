d48e51ae72dc2b3c657ea3beab67a0e55113b842
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-13 20:33:54 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,13 +1,42 @@
-SELECT m.MEMBER_NAME, r.REVIEW_TEXT, DATE_FORMAT(r.REVIEW_DATE,'%Y-%m-%d') AS REVIEW_DATE
-FROM MEMBER_PROFILE m
-INNER JOIN REST_REVIEW r
-ON m.MEMBER_ID = r.MEMBER_ID AND m.MEMBER_ID IN 
-(select MEMBER_ID from (
-    select r.MEMBER_ID,
-        RANK() OVER (ORDER BY COUNT(*) DESC) as ranking
-    from REST_REVIEW r
-    group by r.MEMBER_ID
-    ) ranked 
-where ranking = 1
+-- 가장 리뷰를 많이 쓴 사람의 리뷰 개수
+# select count(*)
+# from REST_REVIEW
+# group by MEMBER_ID
+# order by count(*) desc
+# limit 1;
+
+# with top_writer as (
+#     select m.MEMBER_ID
+#     from MEMBER_PROFILE m
+#     join REST_REVIEW r on m.MEMBER_ID = r.MEMBER_ID
+#     group by m.MEMBER_ID
+#     having count(m.MEMBER_ID) =  ( 
+#         select count(*)
+#         from REST_REVIEW
+#         group by MEMBER_ID
+#         order by count(*) desc
+#         limit 1
+#     )   
+# )
+
+select m.MEMBER_NAME MEMBER_NAME, r.REVIEW_TEXT REVIEW_TEXT, date_format(r.REVIEW_DATE, '%Y-%m-%d') REVIEW_DATE
+from MEMBER_PROFILE m 
+join REST_REVIEW r on m.MEMBER_ID = r.MEMBER_ID
+# from top_writer t
+# join REST_REVIEW r on t.member_id = r.member_id
+where r.MEMBER_ID in (
+    select m.MEMBER_ID
+    from MEMBER_PROFILE m
+    join REST_REVIEW r on m.MEMBER_ID = r.MEMBER_ID
+    group by m.MEMBER_ID
+    having count(m.MEMBER_ID) =  ( 
+        select count(*)
+        from REST_REVIEW
+        group by MEMBER_ID
+        order by count(*) desc
+        limit 1
+    )
 )
-ORDER BY r.REVIEW_DATE ASC, r.REVIEW_TEXT ASC;
+order by REVIEW_DATE, REVIEW_TEXT
+
+
