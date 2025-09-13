dd3fe8c3fd6e03c84cad2b3300a388a8addff4d4
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-13 22:26:39 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,37 +1,19 @@
--- 5월 생산 날짜를 년월까지만으로 바꾸기
+-- 5월 생산 날짜를 년월까지만 보고 각 제품별로 총 주문 개수 구하기
 with 
-may as (
-    select ORDER_ID, PRODUCT_ID, AMOUNT, date_format(PRODUCE_DATE, '%Y-%m') pd
-    from FOOD_ORDER f
-),
--- 5월달 제품별로 총 개수 구하기
-mayTotal as (
+MAY as (
     select PRODUCT_ID, sum(AMOUNT) AMOUNT
-    from may
-    where pd= '2022-05'
+    from FOOD_ORDER
+    where date_format(PRODUCE_DATE, '%Y-%m') = '2022-05'
     group by PRODUCT_ID
 )
 
+# select * from may;
+
+-- 이미 5월에 생산된 총 개수를 구해놨으므로, 그냥 그 식품의 가격과 곱하기만 하면 됨
 select
-o.PRODUCT_ID PRODUCT_ID, p.PRODUCT_NAME PRODUCT_NAME, p.PRICE*o.AMOUNT TOTAL_SALES
-from mayTotal o
-join FOOD_PRODUCT p on o.PRODUCT_ID = p.PRODUCT_ID
+    f.PRODUCT_ID PRODUCT_ID,
+    f.PRODUCT_NAME PRODUCT_NAME,
+    f.PRICE*m.AMOUNT TOTAL_SALES
+from FOOD_PRODUCT f 
+join MAY m on f.PRODUCT_ID = m.PRODUCT_ID
 order by TOTAL_SALES desc, PRODUCT_ID
-# select * 
-# from mayTotal o
-# join FOOD_PRODUCT p on o.PRODUCT_ID = p.PRODUCT_ID;
-
-# select * from FOOD_PRODUCT;
-# select * from FOOD_ORDER
-# select * from FOOD_ORDER;
-
-# select 
-# # * 
-# o.PRODUCT_ID
-# # p.PRODUCT_NAME
-# , sum(p.PRICE * o.AMOUNT)
-# from FOOD_PRODUCT p
-# join FOOD_ORDER o on p.PRODUCT_ID = o.PRODUCT_ID
-# where date_format(o.PRODUCE_DATE, '%Y-%m') = '2022-05'
-# group by o.PRODUCT_ID, o.PRODUCE_DATE
-# having date_format(o.PRODUCE_DATE, '%Y-%m') = '2022-05';
