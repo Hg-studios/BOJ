4128bf213d474925a6350ceb3db7a36d2dace856
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-13 23:37:11 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,8 +1,18 @@
 -- 개발자의 스킬들을 뽑기
+# select d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+# from SKILLCODES s
+# join DEVELOPERS d
+# where (s.code & d.skill_code) = s.code
+# and s.CATEGORY = 'Front End'
+# group by d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+# order by d.ID
+
+
 select d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
-from SKILLCODES s
-join DEVELOPERS d
-where (s.code & d.skill_code) = s.code
-and s.CATEGORY = 'Front End'
-group by d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+from DEVELOPERS d
+where d.SKILL_CODE & (
+    select sum(CODE)
+    from SKILLCODES
+    where CATEGORY='Front End'
+) > 0
 order by d.ID
