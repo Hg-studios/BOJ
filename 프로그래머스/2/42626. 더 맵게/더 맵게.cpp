b2d8da4a29e424c7d4ece99ac4e38232a1d094fc
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-09-14 13:25:41 +0900

    [level 2] Title: 더 맵게, Time: 220.31 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -4,28 +4,35 @@
 #include <iostream>
 using namespace std;
 
+#define ll long long
+
 int solution(vector<int> scoville, int K) {
     int answer = 0;
     
-    priority_queue<int, vector<int>, greater<int>> q;
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
     
-    for(int x : scoville) q.push(x);
-    
-    while(!q.empty() && q.top()<K) {
-        if(q.size()<2) {
-            answer = -1;
-            break;
+    while(1) {
+        if(pq.top()>=K) {
+            return answer;
         }
         
+        // 아직 모든 음식의 스코빌 지수가 K 이상이 안됐다면
+        if(pq.empty()) {
+            return -1;
+        }
+        int x = pq.top();
+        pq.pop();
+        
+        if(pq.empty()) {
+            return -1;
+        }
+        int y = pq.top();
+        pq.pop();
+        
+        pq.push(x+y*2);
         answer++;
-        
-        int first = q.top();
-        q.pop();
-        int second = q.top();
-        q.pop();
-        
-        q.push(first+second*2);
     }
+    
     
     return answer;
 }
