9035fa2a6a8963b4ab6e5316aedc7962b3f6f5d0
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-10-01 19:34:10 +0900

    [level 2] Title: 주식가격, Time: 27.08 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -7,21 +7,23 @@ using namespace std;
 vector<int> solution(vector<int> prices) {
     int n = prices.size();
     vector<int> ans(n, 0);
-    stack<int> st; // 아직 떨어지는 시점을 못 만난 인덱스들
-
-    for (int i = 0; i < n; ++i) {
-        // 새 가격이 들어왔을 때, 이전들보다 작아지면 답 확정
-        while (!st.empty() && prices[i] < prices[st.top()]) {
-            int idx = st.top(); st.pop();
-            ans[idx] = i - idx; // 처음 떨어진 순간까지의 시간
+    stack<int> s;
+    
+    for(int i=0; i<n; i++) {
+        while(!s.empty() && prices[s.top()]>prices[i]) {
+            // 끝나기 전에 뽑힌 경우
+            // 이전에 스택에 쌓인 인덱스의 값을 결정해줌
+            ans[s.top()] = (i-s.top());
+            s.pop();
         }
-        st.push(i);
+        s.push(i);
     }
-
-    // 끝까지 안 떨어진 애들 처리
-    while (!st.empty()) {
-        int idx = st.top(); st.pop();
-        ans[idx] = (n - 1) - idx;
+    
+    // 아직 뽑히지 않은 인덱스들은 끝까지 떨어지는 가격이 없었던 것임
+    while(!s.empty()) {
+        ans[s.top()] = n-1-s.top();
+        s.pop();
     }
+    
     return ans;
 }
