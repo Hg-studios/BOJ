361b09847cfa474eb8c6c3391fd11816384be3d1
Hgstudio <87413634+Hg-studios@users.noreply.github.com>
2025-10-10 17:31:04 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -1,37 +1,27 @@
-#include <string>
+#include <string> 
 #include <vector>
 #include <map>
 using namespace std;
-
 int solution(string skill, vector<string> skill_trees) {
     int answer = 0;
     
-    // 전형적인 위상정렬
-    
-    // income 배열이 필요함
-    // bfs 시작 전에 income 을 채우고, 실제로 이 값이 오면 -- 했던 거 같음 .... 
-    // 근데 이러면 B한테 오는 경우가 C가 아니라 A가 와도 count만 하는 거라면 1로 되는디 .....?
-    
     map<char, int> m;
-    int turn=1;
-    for(auto cur : skill) m[cur] = turn++; // map에 skill의 순서를 저장함
+    int turn=0;
     
-    for(auto cur : skill_trees) {
-        int turn=1;
+    for(auto cur : skill) m [cur]=++turn;
+    
+    for(auto skill_tree : skill_trees) {
+        int cnt=0; // skill 안에 있는 문자만 cnt 함
         bool flag=true;
-        for(auto s : cur) {
-            if(skill.find(s)!=-1) { // skill 에 포함된 스킬이라면
-                if(m[s]!=turn) {
-                    flag = false;
-                    break;
-                }
-                else {
-                    turn++;
-                }
+        for(auto cur: skill_tree) {
+            if(m[cur]==0) continue; // 선행스킬이 아니면 그냥
+            if(m[cur]!=++cnt) { // 만약 선행스킬 순서가 틀리면 다음 단어를 봄
+                flag = false;
+                break;
             }
         }
+    
         if(flag) answer++;
     }
-    
     return answer;
 }
